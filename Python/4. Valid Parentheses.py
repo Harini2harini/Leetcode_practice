aa5058947a995ea4b60b58a62cb34816994c341a
Harini <harini2510m@gmail.com>
2026-03-09 19:26:10 +0530

    Create 4. Valid Parentheses.py

---
+++ Python/4. Valid Parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {')':'(', ']':'[', '}':'{'}
+        for char in s:
+            if char in pairs.values():   
+                stack.append(char)
+            elif char in pairs:          
+                if not stack or stack.pop() != pairs[char]:
+                    return False
+        return len(stack) == 0  
+        
